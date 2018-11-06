a3552c6e38ba38af2c93c4d60dede2dde9e53c7c
Victor de Francisco <vfrancisco@idealista.com>
2018-11-06 22:02:37 +0100

    Recover transaction add-on

--- galileo-android/src/main/java/com/josedlpozo/galileo/parent/home/HomeActivity.kt
+++ galileo-android/src/main/java/com/josedlpozo/galileo/parent/home/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.josedlpozo.galileo.parent.home
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.josedlpozo.galileo.R
+import com.josedlpozo.galileo.chuck.internal.ui.TransactionListView
 import com.josedlpozo.galileo.lynx.GalileoLynx
 import com.josedlpozo.galileo.preferator.Preferator
 import com.josedlpozo.galileo.preferator.model.PreferatorConfig
@@ -16,10 +17,10 @@ internal class HomeActivity : AppCompatActivity() {
         if (savedInstanceState == null) {
             val homeFragment = HomeFragment.newInstance()
             homeFragment.items = listOf(Preferator.view(this, PreferatorConfig()), GalileoLynx(this),
-                RealmView(this))
+                TransactionListView(this), RealmView(this))
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, homeFragment)
-                    .commitNow()
+                .replace(R.id.container, homeFragment)
+                .commitNow()
         }
     }
 
