18577c247688109dd52df36cef84a23a40753224
Victor de Francisco <vfrancisco@idealista.com>
2018-11-07 18:54:35 +0100

    Fix share Realm file action. Bad authority when getting Uri for realm file

--- galileo/src/main/java/com/josedlpozo/galileo/realm/realmbrowser/models/view/ModelsActivity.java
+++ galileo/src/main/java/com/josedlpozo/galileo/realm/realmbrowser/models/view/ModelsActivity.java
@@ -55,11 +55,11 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class ModelsActivity extends AppCompatActivity implements ModelsContract.View, SearchView.OnQueryTextListener {
+
     private ModelsAdapter adapter;
     private SwipeRefreshLayout swipeRefreshLayout;
     private ModelsContract.Presenter presenter;
     private MenuItem sortMenuItem;
-
     private static final String SEARCH_KEY = "search";
     private static final String FILE_NAME_KEY = "file_name";
     private SearchView searchView;
@@ -72,8 +72,7 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         return intent;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.realm_browser_ac_recycler);
 
@@ -83,16 +82,14 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         }
         swipeRefreshLayout = findViewById(R.id.swiperefresh);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
+            @Override public void onRefresh() {
                 presenter.requestForContentUpdate();
             }
         });
         swipeRefreshLayout.setColorSchemeResources(R.color.realm_browser_dark_purple);
 
         adapter = new ModelsAdapter(new ArrayList<>(), new ModelsAdapter.OnModelSelectedListener() {
-            @Override
-            public void onModelSelected(ModelPojo file) {
+            @Override public void onModelSelected(ModelPojo file) {
                 ModelsActivity.this.presenter.onModelSelected(file);
             }
         });
@@ -109,19 +106,16 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         presenter.requestForContentUpdate();
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    @Override protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(SEARCH_KEY, searchString);
     }
 
-    @Override
-    public Object onRetainCustomNonConfigurationInstance() {
+    @Override public Object onRetainCustomNonConfigurationInstance() {
         return presenter;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    @Override public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.realm_browser_menu_modelsactivity, menu);
 
@@ -133,10 +127,9 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         return true;
     }
 
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
+    @Override public boolean onPrepareOptionsMenu(Menu menu) {
         if (searchString != null && !searchString.isEmpty()) {
-            ((SearchView) menu.findItem(R.id.realm_browser_action_filter).getActionView()).setIconified(false);
+            ( (SearchView) menu.findItem(R.id.realm_browser_action_filter).getActionView() ).setIconified(false);
             searchView.requestFocus();
             searchView.setQuery(searchString, true);
             searchView.clearFocus();
@@ -144,8 +137,7 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         return super.onPrepareOptionsMenu(menu);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    @Override public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.realm_browser_action_sort) {
             presenter.onSortModeChanged();
             return true;
@@ -160,8 +152,7 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         }
     }
 
-    @Override
-    public void updateWithModels(@NonNull ArrayList<ModelPojo> filesList, @ModelsContract.SortMode int sortedMode) {
+    @Override public void updateWithModels(@NonNull ArrayList<ModelPojo> filesList, @ModelsContract.SortMode int sortedMode) {
         adapter.swapList(filesList);
         if (sortedMode == ModelsContract.SortMode.ASC && sortMenuItem != null) {
             this.sortMenuItem.setIcon(ContextCompat.getDrawable(this, R.drawable.realm_browser_ic_sort_ascending_white_24dp));
@@ -173,13 +164,11 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         }
     }
 
-    @Override
-    public Context getViewContext() {
+    @Override public Context getViewContext() {
         return this;
     }
 
-    @Override
-    public void attachPresenter(@Nullable ModelsContract.Presenter presenter) {
+    @Override public void attachPresenter(@Nullable ModelsContract.Presenter presenter) {
         this.presenter = presenter;
         if (this.presenter == null) {
             this.presenter = new ModelsPresenter();
@@ -187,9 +176,9 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         this.presenter.attachView(this);
     }
 
-    @Override
-    public void presentShareDialog(@NonNull String path) {
-        Uri contentUri = FileProvider.getUriForFile(this, String.format("%s.share", this.getPackageName()), new File(path));
+    @Override public void presentShareDialog(@NonNull String path) {
+        Uri contentUri = FileProvider.getUriForFile(this, String.format(this.getApplicationInfo().packageName + ".com.josedlpozo.galileo.provider"),
+                                                                        new File(path));
         Intent intentShareFile = new Intent(Intent.ACTION_SEND);
         intentShareFile.setType("application/*");
         intentShareFile.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
@@ -197,24 +186,22 @@ public class ModelsActivity extends AppCompatActivity implements ModelsContract.
         startActivity(Intent.createChooser(intentShareFile, "Share Realm File"));
     }
 
-    @Override
-    public void showInformation(@NonNull InformationPojo informationPojo) {
-        Toast.makeText(this, String.format("%s\nSize: %s", informationPojo.getPath(), Formatter.formatShortFileSize(this, informationPojo.getSizeInByte())), Toast.LENGTH_LONG).show();
+    @Override public void showInformation(@NonNull InformationPojo informationPojo) {
+        Toast.makeText(this,
+                       String.format("%s\nSize: %s", informationPojo.getPath(), Formatter.formatShortFileSize(this, informationPojo.getSizeInByte())),
+                       Toast.LENGTH_LONG).show();
     }
 
-    @Override
-    protected void onDestroy() {
+    @Override protected void onDestroy() {
         super.onDestroy();
         presenter.detachView();
     }
 
-    @Override
-    public boolean onQueryTextSubmit(String query) {
+    @Override public boolean onQueryTextSubmit(String query) {
         return false;
     }
 
-    @Override
-    public boolean onQueryTextChange(String newText) {
+    @Override public boolean onQueryTextChange(String newText) {
         this.searchString = newText;
         presenter.onFilterChanged(newText);
         return true;
