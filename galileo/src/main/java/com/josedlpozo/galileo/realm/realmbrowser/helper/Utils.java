35474660b3d00dcbab15b0e424016ffdd3be5047
Victor de Francisco <vfrancisco@idealista.com>
2018-11-08 21:07:21 +0100

    Include some unit tests for realm data type utilities

--- galileo/src/main/java/com/josedlpozo/galileo/realm/realmbrowser/helper/Utils.java
+++ galileo/src/main/java/com/josedlpozo/galileo/realm/realmbrowser/helper/Utils.java
@@ -30,17 +30,14 @@ package com.josedlpozo.galileo.realm.realmbrowser.helper;
 import android.graphics.Typeface;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.annotation.RestrictTo;
 import android.text.SpannableString;
 import android.text.style.StyleSpan;
-
-import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.util.Date;
-
 import io.realm.DynamicRealmObject;
 import io.realm.RealmObject;
 import io.realm.RealmObjectSchema;
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.util.Date;
 
 public class Utils {
 
