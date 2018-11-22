f074fd0104def84a64c87bfc4f6c2cf9e73f9f72
jmdelpozo <jmdelpozo@idealista.com>
2018-11-22 13:27:03 +0100

    Remove cyanogen

--- galileo/src/main/java/com/josedlpozo/galileo/picker/qs/ColorPickerQuickSettingsTile.java
+++ galileo/src/main/java/com/josedlpozo/galileo/picker/qs/ColorPickerQuickSettingsTile.java
@@ -18,14 +18,10 @@
 package com.josedlpozo.galileo.picker.qs;
 
 import android.app.PendingIntent;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import com.josedlpozo.galileo.R;
-import com.josedlpozo.galileo.picker.utils.LaunchUtils;
 import com.josedlpozo.galileo.picker.utils.PreferenceUtils;
-import cyanogenmod.app.CMStatusBarManager;
-import cyanogenmod.app.CustomTile;
 
 public class ColorPickerQuickSettingsTile {
 
@@ -43,34 +39,12 @@ public class ColorPickerQuickSettingsTile {
         intent.putExtra(OnOffTileState.EXTRA_STATE, state);
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         int iconResId = state == OnOffTileState.STATE_OFF ? R.drawable.ic_qs_colorpicker_off : R.drawable.ic_qs_colorpicker_on;
-        CustomTile tile = new CustomTile.Builder(context).setOnClickIntent(pi)
-                                                         .setLabel(context.getString(R.string.color_picker_qs_tile_label))
-                                                         .setIcon(iconResId)
-                                                         .build();
-        CMStatusBarManager.getInstance(context).publishTile(TAG, TILE_ID, tile);
         PreferenceUtils.ColorPickerPreferences.setColorPickerQsTileEnabled(context, true);
     }
 
     public static void unpublishColorPickerTile(Context context) {
-        CMStatusBarManager.getInstance(context).removeTile(TAG, TILE_ID);
         PreferenceUtils.ColorPickerPreferences.setColorPickerQsTileEnabled(context, false);
         Intent intent = new Intent(ColorPickerQuickSettingsTile.ACTION_UNPUBLISH);
         context.sendBroadcast(intent);
-    }
-
-    public static class ClickBroadcastReceiver extends BroadcastReceiver {
-
-        @Override public void onReceive(Context context, Intent intent) {
-            if (PreferenceUtils.ColorPickerPreferences.getColorPickerQsTileEnabled(context, false)) {
-                int state = intent.getIntExtra(OnOffTileState.EXTRA_STATE, OnOffTileState.STATE_OFF);
-                if (state == OnOffTileState.STATE_OFF) {
-                    publishColorPickerTile(context, OnOffTileState.STATE_ON);
-                    LaunchUtils.startColorPickerOrRequestPermission(context);
-                } else {
-                    publishColorPickerTile(context, OnOffTileState.STATE_OFF);
-                    PreferenceUtils.ColorPickerPreferences.setColorPickerActive(context, false);
-                }
-            }
-        }
     }
 }
