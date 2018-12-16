106124811ceb8345bb64af8d30a53d2c8dfcfe0a
josedlpozo <josedlpozo@gmail.com>
2018-12-16 22:52:13 +0100

    Last attempt

--- galileo/src/androidTest/java/com/josedlpozo/galileo/realm/RealmSnapshooterTest.kt
+++ galileo/src/androidTest/java/com/josedlpozo/galileo/realm/RealmSnapshooterTest.kt
@@ -26,19 +26,12 @@ class RealmSnapshooterTest {
             }
             close()
         }
-        with(Realm.getInstance(builder.name("database_teams").deleteRealmIfMigrationNeeded().build())) {
-            executeTransaction {
-                teams.forEach { copyToRealmOrUpdate(it) }
-            }
-            close()
-        }
 
-        val snapshoot = realmSnapshooter.shoot(listOf(RealmFile("database_developers", "0", 0L),
-            RealmFile("database_teams", "0", 0L)))
+        val snapshoot = realmSnapshooter.shoot(listOf(RealmFile("database_developers", "0", 0L)))
 
-        val expected = "There are 2 Realm databases used by this app\n" +
+        val expected = "There are 1 Realm databases used by this app\n" +
             "\n" +
-            "Database database_developers has 2 tables\n" +
+            "Database database_developers has 1 tables\n" +
             "- DeveloperModel has 2 columns and 6 rows\n" +
             "| id || name |\n" +
             "0 fpulido \n" +
@@ -47,17 +40,6 @@ class RealmSnapshooterTest {
             "3 jaznar \n" +
             "4 molmedo \n" +
             "5 efau \n" +
-            "- TeamModel has 2 columns and 0 rows\n" +
-            "| id || name |\n" +
-            "\n" +
-            "\n" +
-            "Database database_teams has 2 tables\n" +
-            "- DeveloperModel has 2 columns and 0 rows\n" +
-            "| id || name |\n" +
-            "- TeamModel has 2 columns and 2 rows\n" +
-            "| id || name |\n" +
-            "0 android \n" +
-            "1 ios \n" +
             "\n" +
             "\n"
 
