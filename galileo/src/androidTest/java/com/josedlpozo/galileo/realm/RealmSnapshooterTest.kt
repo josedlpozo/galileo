54e12a2f45007bd4c6204dff1b54737fe014286e
josedlpozo <josedlpozo@gmail.com>
2018-12-17 20:54:02 +0100

    Last attempt

--- galileo/src/androidTest/java/com/josedlpozo/galileo/realm/RealmSnapshooterTest.kt
+++ galileo/src/androidTest/java/com/josedlpozo/galileo/realm/RealmSnapshooterTest.kt
@@ -35,32 +35,32 @@ class RealmSnapshooterTest {
         }
 
         val snapshoot = realmSnapshooter.shoot(listOf(RealmFile("database_developers", "0", 0L),
-            RealmFile("database_teams", "0", 0L)))
+                RealmFile("database_teams", "0", 0L)))
 
         val expected = "There are 2 Realm databases used by this app\n" +
-            "\n" +
-            "Database database_developers has 2 tables\n" +
-            "- DeveloperModel has 2 columns and 6 rows\n" +
-            "| id || name |\n" +
-            "0 fpulido \n" +
-            "1 jmdelpozo \n" +
-            "2 vfrancisco \n" +
-            "3 jaznar \n" +
-            "4 molmedo \n" +
-            "5 efau \n" +
-            "- TeamModel has 2 columns and 0 rows\n" +
-            "| id || name |\n" +
-            "\n" +
-            "\n" +
-            "Database database_teams has 2 tables\n" +
-            "- DeveloperModel has 2 columns and 0 rows\n" +
-            "| id || name |\n" +
-            "- TeamModel has 2 columns and 2 rows\n" +
-            "| id || name |\n" +
-            "0 android \n" +
-            "1 ios \n" +
-            "\n" +
-            "\n"
+                "\n" +
+                "Database database_developers has 2 tables\n" +
+                "- TeamModel has 2 columns and 0 rows\n" +
+                "| id || name |\n" +
+                "- DeveloperModel has 2 columns and 6 rows\n" +
+                "| id || name |\n" +
+                "0 fpulido \n" +
+                "1 jmdelpozo \n" +
+                "2 vfrancisco \n" +
+                "3 jaznar \n" +
+                "4 molmedo \n" +
+                "5 efau \n" +
+                "\n" +
+                "\n" +
+                "Database database_teams has 2 tables\n" +
+                "- TeamModel has 2 columns and 2 rows\n" +
+                "| id || name |\n" +
+                "0 android \n" +
+                "1 ios \n" +
+                "- DeveloperModel has 2 columns and 0 rows\n" +
+                "| id || name |\n" +
+                "\n" +
+                "\n"
 
         assertEquals(expected, snapshoot)
     }
