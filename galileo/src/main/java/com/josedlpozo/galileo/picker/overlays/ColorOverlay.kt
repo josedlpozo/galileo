b0c36eef327644133d5f35ad3cf458b25c807601
Fran Pulido <fpulido@idealista.com>
2019-06-18 12:35:27 +0200

    Fixed grid overlay
    - Need to fix picker

---
+++ galileo/src/main/java/com/josedlpozo/galileo/picker/overlays/ColorOverlay.kt
@@ -0,0 +1,11 @@
+package com.josedlpozo.galileo.picker.overlays
+
+import android.view.View
+import com.josedlpozo.galileo.common.BaseFloatItem
+
+class ColorOverlay  : BaseFloatItem(){
+
+    override fun onViewCreated(view: View) {
+
+    }
+}
